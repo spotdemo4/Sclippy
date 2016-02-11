f246e87d9190f9a54781ffae6690650b674e57d2
Trevor Allen <racierjet@hotmail.com>
2016-02-11 16:28:40 -0500

    Idk I just added it

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApplication3 {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 95);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Use ALT + Arrow to move through history";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Clippy";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(180, 9);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 113);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3 {
+    public partial class Form1 : Form {
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string sClassName, String sAppName);
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        public int ayylmao = 0;
+        public int attlmao = 0;
+        string rumorticksit = "";
+        string otherrumor = "";
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m) {
+            if(m.Msg == 0x0312) {
+                int id = m.WParam.ToInt32();
+                if(id == 1) {
+                    label1.Text = listBox1.GetItemText(listBox1.Items[attlmao]);
+                    listBox1.SelectedIndex = attlmao;
+                    this.WindowState = FormWindowState.Minimized;
+                    this.WindowState = FormWindowState.Maximized;
+                    if (ayylmao == 0) {
+                        label1.Visible = true;
+                        ayylmao = 1;
+                    }
+                    else {
+                        label1.Visible = false;
+                        ayylmao = 0;
+                    }
+                }
+                else if (id == 2 && ayylmao == 1) {
+                    try {
+                        attlmao = attlmao - 1;
+                        label1.Text = listBox1.GetItemText(listBox1.Items[attlmao]);
+                        listBox1.SelectedIndex = attlmao;
+                    } catch(Exception e) {
+                        attlmao = attlmao + 1;
+                    }
+                }
+                else if (id == 3 && ayylmao == 1) {
+                    try {
+                        attlmao = attlmao + 1;
+                        label1.Text = listBox1.GetItemText(listBox1.Items[attlmao]);
+                        listBox1.SelectedIndex = attlmao;
+                    }
+                    catch (Exception e) {
+                        attlmao = attlmao - 1;
+                    }
+                }
+                else if (id == 4 && ayylmao == 1) {
+                    Clipboard.SetText(listBox1.GetItemText(listBox1.Items[attlmao]));
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        public enum fsModifiers {
+            Alt = 0x0001,
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.ShowInTaskbar = false;
+            IntPtr thisWindow = FindWindow(null, "Form1");
+            RegisterHotKey(thisWindow, 1, (uint)fsModifiers.Alt, (uint)Keys.G);
+            RegisterHotKey(thisWindow, 2, (uint)fsModifiers.Alt, (uint)Keys.Right);
+            RegisterHotKey(thisWindow, 3, (uint)fsModifiers.Alt, (uint)Keys.Left);
+            RegisterHotKey(thisWindow, 4, (uint)fsModifiers.Alt, (uint)Keys.Enter);
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            TransparencyKey = Color.Gray;
+            this.BackColor = Color.Gray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(0, 0);
+            this.TopMost = true;
+            label1.ForeColor = Color.White;
+            Thread thread = new Thread(dodadoo);
+            thread.Start();
+            label1.Visible = false;
+        }
+        public Color lerpedColor = Color.White;
+
+        public void dodadoo() {
+            while (true) {
+                for (int i = 255; i >= 0; i--) {
+                    label1.ForeColor = Color.FromArgb(i, 255, 255);
+                    System.Threading.Thread.Sleep(5);
+                }
+                for(int i = 0; i < 255; i++) {
+                    label1.ForeColor = Color.FromArgb(i, 255, 255);
+                    System.Threading.Thread.Sleep(5);
+                }
+                //for (int i = 0; i < 255; i++) {
+                //    label1.ForeColor = Color.FromArgb(255, i, 0);
+                //    System.Threading.Thread.Sleep(5);
+                //}
+                //for (int i = 0; i < 255; i++) {
+                //    label1.ForeColor = Color.FromArgb(255, 255, i);
+                //    System.Threading.Thread.Sleep(5);
+                //}
+                //for (int i = 255; i >= 0; i--) {
+                //    label1.ForeColor = Color.FromArgb(i, 255, 255);
+                //    System.Threading.Thread.Sleep(5);
+                //}
+                //for (int i = 255; i >= 0; i--) {
+                //    label1.ForeColor = Color.FromArgb(0, i, 255);
+                //    System.Threading.Thread.Sleep(5);
+                //}
+                //for (int i = 255; i >= 0; i--) {
+                //    label1.ForeColor = Color.FromArgb(0, 0, i);
+                //    System.Threading.Thread.Sleep(5);
+                //}
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            IntPtr thisWindow = FindWindow(null, "Form1");
+            UnregisterHotKey(thisWindow, 1);
+            System.Environment.Exit(1);
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e) {
+            IntPtr thisWindow = FindWindow(null, "Form1");
+            UnregisterHotKey(thisWindow, 1);
+            System.Environment.Exit(1);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            otherrumor = rumorticksit;
+            rumorticksit = Clipboard.GetText();
+            if(rumorticksit.Replace(" ", "") == "") {
+                return;
+            }
+            else if(otherrumor == rumorticksit) {} else {
+                listBox1.Items.Insert(0, rumorticksit);
+            }
+        }
+    }
+}
